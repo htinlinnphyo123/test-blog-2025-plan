bfc27b24f079b4ff7c17dff65f006a722fd841b3
Htin Linn Phyo <htinlinnphyo.npt@gmail.com>
2025-04-14 01:20:32 +0630

    add contact form backend api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use BasicDashboard\Spa\ContactForm\Controllers\ContactFormController;
 
 Route::prefix('v1/spa')->middleware(['accept.json'])->group(function () {
     // require __DIR__ . "/Spa/Category/categoryApi.php";
     require __DIR__ . "/Spa/Article/articleApi.php";
+    Route::post('contactforms', [ContactFormController::class, 'store'])->name('contactForm.store');
 });
